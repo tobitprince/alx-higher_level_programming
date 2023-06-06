1fc6f99e13873d8aa37f0c463dc01e9eeb6c798f
Tobit Prince <68125101+tobitprince@users.noreply.github.com>
2023-06-07 00:24:09 +0300

    Update 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-for i in range(0, 9):
-    for j in range(i + 1, 9):
-        print("{:d}{:d}".format(i, j), end=",")
-print("{:d}".format(79), end=",")
+
+for i in range(9):
+
+    for j in range(i + 1, 10):
+
+        if i * 10 + j < 89:
+
+            print("{:d}{:d}".format(i, j), end=", ")
+
 print("{:d}".format(89))
